65428f3385d6e283fde6a37beccd309257666a79
Kamil <kjas92@gmail.com>
2023-01-26 00:14:43 +0100

    fix editing and removing rows

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -4,7 +4,7 @@
         <nav>
             <Search type="text" placeholder="Filter by product's name" v-on:search="filterChange"/>
         </nav>
-        <CustomTable :headers="headers" actionButtons=false :data="getVisibleData" :row="row"/>
+        <CustomTable :headers="headers" actionButtons=false :data="getVisibleData" :row="row" v-on:edit="edit" v-on:remove="remove"/>
         <button class="add" v-on:click="addProduct">&plus;</button>
     </div>       
 </template>
